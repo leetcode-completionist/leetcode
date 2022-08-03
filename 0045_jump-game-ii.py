79dfe303f2eea9f472ed152dc95f9968520a25f3
leetcode-completionist <110565757+leetcode-completionist@users.noreply.github.com>
2022-08-03 12:46:54 -0700

    Update 0045_jump-game-ii.py
    fix implementation

--- 0045_jump-game-ii.py
+++ 0045_jump-game-ii.py
@@ -1,16 +1,17 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        if len(nums) == 1:
+        n = len(nums)
+        if n == 1:
             return 0
 
-        dp = [0] * len(nums)        
-        for i in range(len(nums) - 2, -1, -1):
+        dp = [0] * n
+        for i in range(n - 2, -1, -1):
             if nums[i] == 0:
                 # the end is unreachable from here
                 dp[i] = float('inf')
                 continue
 
-            min_jump = min(dp[i+1:i+1+nums[i]])
-            
+            min_jumps = min(dp[i+1:i+1+nums[i]])
+            dp[i] = 1 + min_jumps
         
         return dp[0]
