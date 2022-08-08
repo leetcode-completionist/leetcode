a6ad683e85304b8f68162e8a3ad2331b641494b2
leetcode-completionist <110565757+leetcode-completionist@users.noreply.github.com>
2022-08-07 17:06:24 -0700

    Update 0181_employees-earning-more-than-their-managers.sql
    Rewrite query into a shorter one

--- 0181_employees-earning-more-than-their-managers.sql
+++ 0181_employees-earning-more-than-their-managers.sql
@@ -1,23 +1,9 @@
 # Write your MySQL query statement below
 SELECT 
-  name as "Employee" 
+  e1.name as "Employee" 
 FROM 
-  Employee e 
-  INNER JOIN (
-    SELECT 
-      id, 
-      salary 
-    FROM 
-      Employee 
-    WHERE 
-      id IN (
-        SELECT 
-          DISTINCT managerId 
-        FROM 
-          Employee 
-        WHERE 
-          managerId IS NOT NULL
-      )
-  ) m ON e.managerId = m.id 
-WHERE 
-  e.salary > m.salary
+  Employee e1,
+  Employee e2
+WHERE
+  e1.managerId = e2.id
+  and e1.salary > e2.salary
