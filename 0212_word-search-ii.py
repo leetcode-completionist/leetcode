18305a42ad4d0b3db8389f107d816b74848028ae
leetcode-completionist <110565757+leetcode-completionist@users.noreply.github.com>
2022-08-11 19:43:32 -0700

    Update 0212_word-search-ii.py
    Simplify Trie and Methods

--- 0212_word-search-ii.py
+++ 0212_word-search-ii.py
@@ -9,11 +9,8 @@ class TrieNode:
 
 class Trie:
     
-    def __init__(self, board: List[List[str]]):
+    def __init__(self):
         self.root = TrieNode()
-        self.board = board
-        self.height = len(self.board)
-        self.width = len(self.board[0])
     
     
     def addWord(self, word: str) -> None:
@@ -23,20 +20,13 @@ class Trie:
                 node.children[c] = TrieNode(val=c, parent=node)
             node = node.children[c]
         node.word = word
-
-
-    def searchWord(self,
-                   x: int,
-                   y: int) -> List[str]:
-        """
-        Given an (i, j) position on the board, we will perform
-        DFS in all four directions (with backtracking) IF
-        the new board position allows us to traverse down our Trie.
         
+    
+    def prune(self, node: TrieNode) -> None:
+        """
         When a word is found, we will perform the following actions:
         
         - Set the word node's "word" to None
-
         - If the current word node's child count is 0, we will
           traverse up node's parent and "evict" the child. We will
           perform this check up to trie until we reach a node who's child
@@ -62,77 +52,58 @@ class Trie:
         Finally, after finding "batter", we will prune the entire
         "bat..." branch.
         """
-        res = []
-        
-        trie_node = self.root
-        
-        seen = set()
-        
-        def dfs(i, j) -> None:
-            nonlocal trie_node
-            
-            if i < 0 or i == self.height or j < 0 or j == self.width:
-                return
-            
-            if (i, j) in seen:
-                return
-            
-            if self.board[i][j] not in trie_node.children:
-                return
-            
-            # we can use current board position to (maybe) build a word
-            seen.add((i, j))
-            trie_node = trie_node.children[self.board[i][j]]
-            
-            if trie_node.word:
-                res.append(trie_node.word)
-                
-                # remove current word from possible match later
-                trie_node.word = None
-                
-                # prune trie until we reach a node with remaining children
-                # or the root
-                child = trie_node
-                parent = child.parent
-                while parent and len(child.children) == 0:
-                    del parent.children[child.val]
-                    child = parent
-                    parent = parent.parent
+        # remove current word from possible match later
+        node.word = None
 
-            if len(trie_node.children) == 0:
-                # no more words can be built from here
-                # no need to dfs further, so we backtrack
-                trie_node = trie_node.parent
-                seen.remove((i, j))
-                return
-                    
-            dfs(i + 1, j)
-            dfs(i - 1, j)
-            dfs(i, j + 1)
-            dfs(i, j - 1)
-            
-            # back track
-            trie_node = trie_node.parent
-            seen.remove((i, j))
-            
-        dfs(x, y)
-        
-        return res
-        
+        # prune trie until we reach a node with remaining children
+        # or the root
+        child = node
+        parent = child.parent
+        while parent and len(child.children) == 0:
+            del parent.children[child.val]
+            child = parent
+            parent = parent.parent
         
         
 class Solution:
     def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
-        trie = Trie(board)
+        trie = Trie()
         for word in words:
             trie.addWord(word)
         
-        m, n = len(board), len(board[0])
-        
         res = []
+        m, n = len(board), len(board[0])
+        seen = set()
+        
+        def dfs(i, j, node) -> None:
+            if (i < 0 or i == m or j < 0 or j == n or
+                (i, j) in seen or board[i][j] not in node.children):
+                return
+            
+            # we can use current board position to (maybe) build a word
+            seen.add((i, j))
+            node = node.children[board[i][j]]
+            
+            if node.word:
+                res.append(node.word)
+                trie.prune(node)
+
+            if len(node.children) == 0:
+                # no more words can be built from here
+                # no need to dfs further, so we backtrack
+                seen.remove((i, j))
+                return
+
+            dfs(i + 1, j, node)
+            dfs(i - 1, j, node)
+            dfs(i, j + 1, node)
+            dfs(i, j - 1, node)
+            
+            # back track
+            seen.remove((i, j))
         
         for i in range(m):
             for j in range(n):
-                res.extend(trie.searchWord(i, j))
+                dfs(i, j, trie.root)
         
         return res
