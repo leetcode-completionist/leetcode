186368bc19b1d80fc19d623758c2456259ea626f
leetcode-completionist <110565757+leetcode-completionist@users.noreply.github.com>
2022-09-10 21:31:43 -0700

    Create 1383_maximum-performance-of-a-team.py

---
+++ 1383_maximum-performance-of-a-team.py
@@ -0,0 +1,36 @@
+# https://leetcode.com/problems/maximum-performance-of-a-team/
+import heapq
+
+class Solution:
+    
+    MOD = 10 ** 9 + 7
+    
+    def maxPerformance(self, n: int, speed: List[int], efficiency: List[int], k: int) -> int:
+        # create a list of engineers (i.e. speed/efficiency tuples)
+        engineers = zip(speed, efficiency)
+        
+        # sort by efficiency in decreasing order.
+        # this makes it so that for every engineer we come across,
+        # we know we have the min efficiency so far
+        engineers = sorted(engineers, key = lambda x: -x[1])
+        
+        res = 0
+        
+        # we keep a min_heap of max size of k
+        speed_heap = []
+        
+        # we keep a running total speed so we can quickly calculate
+        # the current performance
+        total_speed = 0
+        
+        for i, (curr_speed, curr_efficiency) in enumerate(engineers):
+            total_speed += curr_speed
+            heapq.heappush(speed_heap, curr_speed)
+
+            if len(speed_heap) > k:
+                # we have more than k engineers, pop the lowest speed
+                total_speed -= heapq.heappop(speed_heap)
+            
+            res = max(res, total_speed * curr_efficiency)
+            
+        return res % Solution.MOD
