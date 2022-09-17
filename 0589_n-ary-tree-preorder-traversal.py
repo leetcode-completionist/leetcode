c7438bbfed8aa85d89c1638c9fca40e8f73e1043
leetcode-completionist <110565757+leetcode-completionist@users.noreply.github.com>
2022-09-16 22:27:37 -0700

    Create 0589_n-ary-tree-preorder-traversal.py

---
+++ 0589_n-ary-tree-preorder-traversal.py
@@ -0,0 +1,25 @@
+# https://leetcode.com/problems/n-ary-tree-preorder-traversal/
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def preorder(self, root: 'Node') -> List[int]:
+        res = []
+        
+        def dfs(node: 'Node') -> None:
+            if not node:
+                return
+            
+            res.append(node.val)
+            
+            for child in node.children:
+                dfs(child)
+        
+        dfs(root)
+        
+        return res
