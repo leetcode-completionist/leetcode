fc8a8a3ca39eda8fb857adecb6fe262cc8cccb12
leetcode-completionist <110565757+leetcode-completionist@users.noreply.github.com>
2022-10-10 20:41:56 -0700

    Create 0334_increasing-triplet-subsequence.py

---
+++ 0334_increasing-triplet-subsequence.py
@@ -0,0 +1,14 @@
+# https://leetcode.com/problems/increasing-triplet-subsequence/
+class Solution:
+    def increasingTriplet(self, nums: List[int]) -> bool:
+        first = second = math.inf
+
+        for num in nums:
+            if num <= first:
+                first = num
+            elif num <= second:
+                second = num
+            else:
+                return True
+        
+        return False
