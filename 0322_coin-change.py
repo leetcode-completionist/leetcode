b6ee33201252409369fb59516a3dd35b0247f97b
leetcode-completionist <110565757+leetcode-completionist@users.noreply.github.com>
2022-10-17 17:17:41 -0700

    Update 0322_coin-change.py
    binary search is not better than linear short-circuiting

--- 0322_coin-change.py
+++ 0322_coin-change.py
@@ -1,28 +1,30 @@
 # https://leetcode.com/problems/coin-change/
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        # Sort coins so we can binary search the coin values
-        # to end our inner loop.
+        # Sort coins so we can short-circuit
+        # our inner loop (below)
         coins.sort()
         
         # Initialize DP array with "-1"
         # Indices map to amounts: $0, $1, ..., $amount
         dp = [-1] * (amount + 1)
         
-        # It requires ZERO coins for $0
+        # It requires "0" coins for $0
         dp[0] = 0
         
         for i in range(1, amount + 1):
             candidates = []
             
-            for j in range(bisect.bisect_right(coins, i)):
-                # try all coins from smallest until largest
-                # while being able to stay <= $amount
-                coin = coins[j]
+            for coin in coins:
+                diff = i - coin
+                if diff < 0:
+                    # short-circuit
+                    break
+                if dp[diff] == -1:
+                    # invalid option
+                    continue
+                candidates.append(dp[diff])
 
-                if dp[i - coin] >= 0:
-                    candidates.append(dp[i - coin])
-                    
             if not candidates:
                 # No valid candidates found,
                 # leave DP cell as "-1"
